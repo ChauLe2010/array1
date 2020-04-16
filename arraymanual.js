92295902a8a4e9bcdf35a19b40b8cd29b8a730ca
chau.le <chau.le@codegym.vn>
2020-04-16 15:39:40 +0700

    init

---
+++ arraymanual.js
@@ -0,0 +1,13 @@
+function inputArray(marks) {
+    alert("fafafa");
+    // let N=+prompt("Nhap so phan tu cua mang: ");
+    // for(let i=0;i<N;i++){
+    //     marks[i]=+prompt("Nhap phan tu thu "+i+": ");
+    // }
+}
+function displayArray(marks){
+    document.write("Mang vua nhap la: ");
+    for(let i=0;i<marks.length;i++){
+        document.write(marks[i]+"  ");
+    }
+}
